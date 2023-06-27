874747cb77294341c0fdce0c8b97f321692df39e
Renz 8:28 <110266073+RenzKing@users.noreply.github.com>
2023-06-27 16:44:57 +0800

    card v2

--- src/index.js
+++ src/index.js
@@ -1,17 +1,78 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const skills = [
+  {
+    skill: "HTML",
+    level: "Intermediate",
+    color: "red",
+  },
+  {
+    skill: "CSS",
+    level: "Intermediate",
+    color: "gree",
+  },
+  {
+    skill: "JAVASCRIPT",
+    level: "Intermediate",
+    color: "yellow",
+  },
+  {
+    skill: "REACT",
+    level: "Intermediate",
+    color: "blue",
+  },
+];
+
+function App() {
+  return (
+    <React.Fragment>
+      <div className="card">
+        <Avatar />
+
+        <Intro />
+        {/* <SkillList /> */}
+      </div>
+    </React.Fragment>
+  );
+}
+
+function Avatar() {
+  return <img src="/pic.jpg" className="avatar" alt="" />;
+}
+
+function Intro() {
+  return (
+    <div className="data">
+      <h1>Renz King Gayacao</h1>
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book.
+      </p>
+    </div>
+  );
+}
+
+// function SkillList (){
+//     return (
+
+//     )
+// }
+
+// function Skill(){
+//     return (
+
+//     )
+// }
+
+//RENDER ROOT//
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
