e2c07e7b6b3e26fe1f42672de09d1188186f998f
Renz 8:28 <110266073+RenzKing@users.noreply.github.com>
2023-06-28 20:07:53 +0800

    card v2 practice

--- src/index.js
+++ src/index.js
@@ -35,58 +35,70 @@ const skills = [
   },
 ];
 
+// function App() {
+//   return (
+//     <div className="card">
+//       <Avatar />
+//       <div className="data">
+//         <Intro />
+//         <SkillList />
+//       </div>
+//     </div>
+//   );
+// }
+
+// function Avatar() {
+//   return <img className="avatar" src="/pic.jpg" alt="Renz" />;
+// }
+
+// function Intro() {
+//   return (
+//     <div>
+//       <h1>Renz King Gayacao</h1>
+//       <p>
+//         Lorem Ipsum is simply dummy text of the printing and typesetting
+//         industry. Lorem Ipsum has been the industry's standard dummy text ever
+//         since the 1500s, when an unknown printer took a galley of type and
+//         scrambled it to make a type specimen book. It has survived not only five
+//         centuries
+//       </p>
+//     </div>
+//   );
+// }
+
+// function SkillList() {
+//   return (
+//     <div className="skill-list">
+//       {skills.map((skill) => (
+//         <Skill skill={skill.skill} level={skill.level} color={skill.color} />
+//       ))}
+//     </div>
+//   );
+// }
+// function Skill({ skill, color, level }) {
+//   return (
+//     <div className="skill" style={{ backgroundColor: color }}>
+//       <span>{skill}</span>
+
+//       <span>
+//         {level === "Beginer" && "😵‍💫"}
+//         {level === "Intermediate" && "👌"}
+//         {level === "Advance" && "💪"}
+//       </span>
+//     </div>
+//   );
+// }
+
 function App() {
-  return (
-    <div className="card">
-      <Avatar />
-      <div className="data">
-        <Intro />
-        <SkillList />
-      </div>
-    </div>
-  );
+  <>
+    <Avatar />
+    <Intro />
+    <SkillList />
+  </>;
 }
 
 function Avatar() {
-  return <img className="avatar" src="/pic.jpg" alt="Renz" />;
-}
-
-function Intro() {
-  return (
-    <div>
-      <h1>Renz King Gayacao</h1>
-      <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries
-      </p>
-    </div>
-  );
-}
-
-function SkillList() {
-  return (
-    <div className="skill-list">
-      {skills.map((skill) => (
-        <Skill skill={skill.skill} level={skill.level} color={skill.color} />
-      ))}
-    </div>
-  );
-}
-function Skill({ skill, color, level }) {
-  return (
-    <div className="skill" style={{ backgroundColor: color }}>
-      <span>{skill}</span>
-
-      <span>
-        {level === "Beginer" && "😵‍💫"}
-        {level === "Intermediate" && "👌"}
-        {level === "Advance" && "💪"}
-      </span>
-    </div>
-  );
+  <img className="avatar" src="/pic.jpg" alt="Renz" />;
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
